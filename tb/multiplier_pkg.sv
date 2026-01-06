9accef28fbe8b868e92a2fd4b14e8a9e238a7bff
Adil Rafiq <165660038+adilrchanna@users.noreply.github.com>
2026-01-06 23:11:53 +0500

    Initial commit

---
+++ tb/multiplier_pkg.sv
@@ -0,0 +1,10 @@
+package multiplier_pkg;
+
+    `include "transaction.sv"
+    `include "generator.sv"
+    `include "driver.sv"
+    `include "monitor.sv"
+    `include "scoreboard.sv"
+    `include "environment.sv"
+
+endpackage
