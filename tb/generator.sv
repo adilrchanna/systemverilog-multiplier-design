7f9ba8470fc3eaa1fb6a31682656c83280461c13
Adil Rafiq <165660038+adilrchanna@users.noreply.github.com>
2026-01-06 23:00:33 +0500

    Ini

---
+++ tb/generator.sv
@@ -0,0 +1,24 @@
+class generator #(parameter N=8);
+    transaction #(N) trans;
+    mailbox gen2drv;
+    event ended;
+    int repeat_count;
+
+    function new(mailbox gen2drv);
+        this.gen2drv = gen2drv;
+    endfunction
+
+    task main();
+        repeat(repeat_count) begin
+        trans = new();
+            
+	      // randomize check
+        if (!trans.randomize()) begin
+            $fatal("[GEN] Transaction randomization failed!");
+        end
+            
+        gen2drv.put(trans);
+        end
+        -> ended; // end of generation
+    endtask
+endclass
